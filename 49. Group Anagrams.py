2a2da2fd644e962f4d8835ba2f0704d7599ff6a3
Maunil <vyasmaunil33@gmail.com>
2020-05-22 19:14:25 -0700

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -11,4 +11,31 @@ class Solution:
 
         return dic.values()
         
+'''
+Solution 2 : Without using sorting. 
+
+class Solution:
+    
+    def key_convert(self, s, key_list):
+        for i in range(len(s)):
+            key_list[ord(s[i])-97] += 1
         
+        return tuple(key_list)
+        
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:        
+        dic = {}
+        
+        for s in strs:
+            key_list = [0 for i in range (26)]
+            key = self.key_convert(s, key_list)
+            
+            #tuple(sorted(s))
+            if key in dic:
+                dic[key].append(s)
+            else:
+                dic[key] = []
+                dic[key].append(s)
+
+        return dic.values()
+        
+'''
