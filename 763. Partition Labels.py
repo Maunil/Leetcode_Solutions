90b841fa9cee2369893ad82795b61757f93c9330
Maunil <vyasmaunil33@gmail.com>
2020-06-03 23:35:12 -0700

    Update 763. Partition Labels.py

--- 763. Partition Labels.py
+++ 763. Partition Labels.py
@@ -1,30 +1,26 @@
 class Solution:
-    
-    def help(self, first_part_str, second_part_str, index):
-        dic = {}
-        for c in first_part_str:
-            if c not in dic:
-                dic[c] = True 
+    def helper(self, index, dic, S):
+        last_index = dic[S[index]]
+        start_point = index 
         
-        count = 0
-        for c in range(len(second_part_str)):
-            if second_part_str[c] in dic:
-                       count = c + 1
-                       
-        return count + index  
-    
+        while index < last_index :
+            if dic[S[index]] > last_index:
+                last_index = dic[S[index]]
+            index += 1 
+        
+        return (last_index - start_point) + 1, last_index + 1  
+        
     def partitionLabels(self, S: str) -> List[int]:
-        index = 1
+        dic = {}
         ans = []
-        prev = 0
         
-        while index <= len(S):
-            next_index = self.help(S[prev:index], S[index:], index)
-            if next_index == index:
-                ans.append(index-prev)
-                prev = index 
-                index +=1
-            else:
-                index = next_index 
-                
-        return ans
+        for i in range(len(S)):
+            dic[S[i]] = i
+        
+        i = 0 
+        
+        while i < len(S):
+            count, i = self.helper(i, dic, S)       
+            ans.append(count)
+            
+        return ans 
