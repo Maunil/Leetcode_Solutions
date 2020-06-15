a6a777110189b91ec4e5d867457fbc533525c4ab
Maunil <vyasmaunil33@gmail.com>
2020-06-15 11:02:13 -0700

    Update Finding the mother vertex in Graph.py

--- Finding the mother vertex in Graph.py
+++ Finding the mother vertex in Graph.py
@@ -3,6 +3,8 @@ Not a leetcode question, time complexity : O (V+E)
 '''
 
 
+
+
 class Graph:
   n = None
   G = None 
@@ -18,8 +20,9 @@ class Graph:
        self.G[start].append(end)
 
   def DFS_util (self, visit, node_key):
-    if node_key in self.G and visit[node_key] == False:
-      for i in range(len(self.G[node_key])):
+    visit[node_key] = True
+    for i in range(len(self.G[node_key])):
+      if visit[self.G[node_key][i]] == False:
         self.DFS_util(visit, self.G[node_key][i])
         visit[self.G[node_key][i]] = True
 
@@ -30,7 +33,6 @@ class Graph:
       if visit[i] == False:
         self.DFS_util(visit, i)
         mother_index = i
-        visit[i] = True
 
     visit = [False]*self.n
     self.DFS_util(visit, mother_index)
@@ -49,12 +51,11 @@ class Graph:
     return True
     
 g = Graph(5)
-g.add_edge(0,1)
-g.add_edge(1,2)
-g.add_edge(3,0)
-g.add_edge(3,1)
+g.add_edge(1,0)
+g.add_edge(0,2)
+g.add_edge(2,1)
+g.add_edge(0,3)
 g.add_edge(3,4)
-g.add_edge(4,1)
 
 
 print ("This Graph structure is : " + str(g.G))
